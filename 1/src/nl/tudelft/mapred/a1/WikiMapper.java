71c9f1e77b41ab50509ff83a60194921e903d202
Dzmitry Zhaleznichenka <Dzmitry.Zhaleznichenka@gmail.com>
2012-02-21 13:43:12 +0100

    Initial Hadoop confiduration

---
+++ 1/src/nl/tudelft/mapred/a1/WikiMapper.java
@@ -0,0 +1,9 @@
+package nl.tudelft.mapred.a1;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class WikiMapper extends Mapper<Object, Text, Text, IntWritable>{
+
+}
