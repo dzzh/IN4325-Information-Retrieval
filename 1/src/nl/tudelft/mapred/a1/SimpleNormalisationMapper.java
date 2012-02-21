59ed0737aa68d7a769bc3942a03400a5761764f5
Dzmitry Zhaleznichenka <Dzmitry.Zhaleznichenka@gmail.com>
2012-02-21 22:00:54 +0100

    Commit forgotten code

---
+++ 1/src/nl/tudelft/mapred/a1/SimpleNormalisationMapper.java
@@ -0,0 +1,56 @@
+package nl.tudelft.mapred.a1;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SimpleNormalisationMapper extends Mapper<Object, Text, Text, IntWritable>{
+	
+	private static final String ID_TAG = "id";
+	private static final String TEXT_TAG = "text";
+	
+	private static final IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SimpleNormalisationMapper.class);
+    
+    public void map(Object key, Text value, Context context) throws IOException, InterruptedException
+    {
+    	//parsing the document contents
+    	String stringValue = value.toString();
+    	
+    	int id = Integer.valueOf(extractContents(stringValue, ID_TAG)).intValue();
+    	LOGGER.info(String.valueOf(id));
+    	String text = extractContents(stringValue, TEXT_TAG);
+    	if (text.length() == 0){
+    		LOGGER.warn("No text was extracted for id " + id);
+    	}
+    	
+        StringTokenizer st = new StringTokenizer(text);
+        while(st.hasMoreTokens())
+        {
+            word.set(st.nextToken());
+            context.write(word,one);
+        }
+    }
+    
+    private String extractContents(String value, String tag){
+    	if(value.length() < 100){
+    		LOGGER.info(value);
+    	}
+    	int start = value.indexOf("<" + tag + ">") + tag.length() + 2;
+    	int end = value.indexOf("</" + tag + ">");
+    	if (start > -1 && end > -1){
+    		return value.substring(start, end);
+    	} else {
+    		LOGGER.warn("Cannot extract contents: tag not found");
+    		return "";
+    	}
+    }
+    
+}
---
+++ 1/src/nl/tudelft/mapred/a1/SimpleNormalisationReducer.java
@@ -0,0 +1,19 @@
+package nl.tudelft.mapred.a1;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class SimpleNormalisationReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+	public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException
+    {
+        int sum = 0;
+        for(IntWritable iw : values)
+        {
+            sum += iw.get();
+        }
+        context.write(key, new IntWritable(sum));
+    }
+}
