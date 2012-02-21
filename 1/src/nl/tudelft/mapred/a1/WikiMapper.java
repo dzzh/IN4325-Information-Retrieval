3b0d4f3101d3a211b0cdd32db228ef17f7c31eee
Dzmitry Zhaleznichenka <Dzmitry.Zhaleznichenka@gmail.com>
2012-02-21 21:45:32 +0100

    Simple normalisation implemented

--- 1/src/nl/tudelft/mapred/a1/WikiMapper.java
+++
@@ -1,9 +0,0 @@
-package nl.tudelft.mapred.a1;
-
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Mapper;
-
-public class WikiMapper extends Mapper<Object, Text, Text, IntWritable>{
-
-}
