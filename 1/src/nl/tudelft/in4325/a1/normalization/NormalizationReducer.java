ee00f22ec777ba28fc469cb19ca360b87efeb867
Dzmitry Zhaleznichenka <Dzmitry.Zhaleznichenka@gmail.com>
2012-02-23 04:08:27 +0100

    Eventually almost final version of assignment 1

--- 1/src/nl/tudelft/in4325/a1/normalization/NormalizationReducer.java
+++ 1/src/nl/tudelft/in4325/a1/normalization/NormalizationReducer.java
@@ -6,19 +6,27 @@ import nl.tudelft.in4325.a1.indexing.TextArrayWritable;
 
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Reducer for Wikipedia XML corpus normalization
  */
 public class NormalizationReducer extends Reducer<Text, TextArrayWritable, Text, IntWritable> {
-	public void reduce(Text key, Iterable<TextArrayWritable> values, Context context) throws IOException, InterruptedException
-    {
-        int sum = 0;
-        for(TextArrayWritable tw : values)
-        {
-            sum += tw.toStrings().length;
-        }
-        context.write(key, new IntWritable(sum));
+	
+	private final Logger LOGGER = LoggerFactory.getLogger(NormalizationReducer.class);
+
+	public void reduce(Text key, Iterable<TextArrayWritable> values, Context context) throws IOException, InterruptedException{
+		int sum = 0;
+		while (values.iterator().hasNext()) {
+			Writable[] value = ((TextArrayWritable) values.iterator().next()).get();
+			Text array = (Text) value[1];
+			sum += array.toString().split(",").length;
+			//LOGGER.info(key + "   " + sum);
+		}
+		
+		context.write(key, new IntWritable(sum));
     }
 }
